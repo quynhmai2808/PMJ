18ad1e62ef95da6e01fcba7ac4c9505ea2631260
quynhmai2808 <49148033+quynhmai2808@users.noreply.github.com>
2026-08-05 17:26:08 +0200

    Update pmg_process.R

--- standard_process/project/funtions/pmg_process.R
+++ standard_process/project/funtions/pmg_process.R
@@ -6,10 +6,10 @@
 find_pmi_files <- function(path_pmi, pmg_stamp, missing_ean_stamp) {
   pmg_product_files <- dir(path_pmi, pattern = "pmg_product", full.names = TRUE)
   pmg_product <- pmg_product_files[stringr::str_detect(basename(pmg_product_files), pmg_stamp)]
-
+  
   missing_ean_files <- dir(path_pmi, pattern = "missing_ean", full.names = TRUE)
   missing_ean <- missing_ean_files[stringr::str_detect(basename(missing_ean_files), missing_ean_stamp)]
-
+  
   list(pmg_product = pmg_product, missing_ean = missing_ean)
 }
 
@@ -22,7 +22,7 @@ read_pmg_product <- function(pmg_product_path, encoding = "UTF-8") {
     na.strings = c("NULL"),
     fileEncoding = encoding
   )
-
+  
   pmi_file_raw %>%
     dplyr::mutate(EAN = nosigns(EAN)) %>%
     dplyr::mutate(EAN = as.character(as.numeric(EAN))) %>%
@@ -55,11 +55,11 @@ read_missing_eans <- function(missing_ean_path, encoding = "UTF-8") {
 #' producing extra rows keyed by the internal article number (acting as a
 #' substitute EAN) for items not already present under their real EAN.
 build_missing_ean_join <- function(pmi_file_raw, missing_eans) {
-
+  
   common_values <- intersect(pmi_file_raw$EAN, missing_eans$INTERNAL_ARTICLE_NR)
   items_without_dup <- missing_eans %>%
     dplyr::filter(!INTERNAL_ARTICLE_NR %in% common_values)
-
+  
   pmi_cols <- c(
     "EANTYPE","PRODUCTNUMBER","VALIDFROM","VALIDTO","DWHPRODUCTID","PRODUCTSTATUS",
     "PRODUCTNAME","SHORTCODE","PRODUCTGROUPL1","PRODUCTGROUPL2","OTP_PACK_CONTENT_G",
@@ -71,7 +71,7 @@ build_missing_ean_join <- function(pmi_file_raw, missing_eans) {
     "CARBONMONOXIDE","CODING","TIPPING","TASTEINTL","LENGTHTYPE","DISTRIBUTIONCATEGORY",
     "THICKNESS","PACKSIZE","PACKTYPE","GIMSBRANDDIFF","GIMSPACKTYPE","GIMSLENGTH","PRICE"
   )
-
+  
   items_without_dup %>%
     dplyr::inner_join(pmi_file_raw, by = c("EAN" = "EAN")) %>%
     dplyr::rename(EAN_OLD = EAN, EAN = INTERNAL_ARTICLE_NR) %>%
@@ -83,26 +83,26 @@ build_missing_ean_join <- function(pmi_file_raw, missing_eans) {
 #'
 #' @return list(pmi_file, check_QC_bundle, check_QC_pack)
 pmg_process <- function(path_pmi, pmg_stamp, missing_ean_stamp, encoding = "UTF-8") {
-
+  
   files <- find_pmi_files(path_pmi, pmg_stamp, missing_ean_stamp)
-
+  
   pmi_file_raw <- read_pmg_product(files$pmg_product, encoding)
   missing_eans <- read_missing_eans(files$missing_ean, encoding)
-
+  
   ean_join <- build_missing_ean_join(pmi_file_raw, missing_eans)
-
+  
   pmi_file <- dplyr::bind_rows(pmi_file_raw, ean_join) %>%
     dplyr::mutate(EAN_fuer_Filter = EAN)
-
+  
   check_QC_bundle <- pmi_file %>%
     dplyr::filter(EANTYPE == "B" & (ITEMSPERBUNDLE == "" | OTP_BUNDLE_CONTENT_G == "")) %>%
     dplyr::filter(PRODUCER == "PMG" | PRODUCTTYPE == "Cigarette" |
                     LENGTHTYPE == "RRP HTP STICKS" | PRODUCTTYPE == "Finecut")
-
+  
   check_QC_pack <- pmi_file %>%
     dplyr::filter(EANTYPE == "P" & (ITEMSPERPACK == "" | OTP_PACK_CONTENT_G == "")) %>%
     dplyr::filter(PRODUCER == "PMG" | PRODUCTTYPE == "Cigarette" |
                     LENGTHTYPE == "RRP HTP STICKS" | PRODUCTTYPE == "Finecut")
-
+  
   list(pmi_file = pmi_file, check_QC_bundle = check_QC_bundle, check_QC_pack = check_QC_pack)
 }
